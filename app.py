95bd91b4b2d4c010d1989ad50f2d70d60c2b5086
arshamkhursheed <arshamkhursheed@gmail.com>
2025-09-22 15:49:49 +0530

    this is new code for add

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])   # Homepage handles GET and POST
 def home():
-    return render_template("index.html")  # Instead of text, give the HTML file
+    result = None
+    if request.method == "POST":    # If form submitted
+        num1 = request.form.get("num1")   # Take first number from input
+        num2 = request.form.get("num2")   # Take second number from input
+        
+        # Convert to integers and add
+        result = int(num1) + int(num2)
+
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
